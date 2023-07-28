d70e2e45ada3d514a8e1a69250fb3a69d2831cee
klei22 <kauna@avalonwest.tech>
2023-07-28 11:39:28 -0700

    Combine work into single template

--- rl_template.py
+++ rl_template.py
@@ -5,16 +5,18 @@ import argparse
 from stable_baselines3 import A2C, PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 
-# Define the function you want to optimize
 def target_function(x):
-    return -((x[0] - 1) **2 + (x[1] + 1)**2 - 0.1*np.sin(5 * x[0]) * 0.1*np.sin(5 * x[1]))  # Made function more complex with local minima
+    return -((x[0] - 1) **2 + (x[1] + 1)**2 - 0.1*np.sin(5 * x[0]) * 0.1*np.sin(5 * x[1]))
 
 class CustomEnv(gym.Env):
-    def __init__(self):
+    def __init__(self, initial_state):
         super(CustomEnv, self).__init__()
+
         self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,))
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(2,))
-        self.state = np.zeros(2)
+
+        # Initialize state with the initial_state argument
+        self.state = np.array(initial_state)
 
     def step(self, action):
         self.state += action
@@ -38,9 +40,16 @@ def main():
     parser = argparse.ArgumentParser(description="Train an agent to maximize a function.")
     parser.add_argument("--model", help="The model to use, either 'ppo' or 'a2c'", type=str, default="ppo")
     parser.add_argument("--max_timesteps", help="The maximum number of timesteps to train for", type=int, default=100000)
+
+    # Add argparse arguments for the initial states of x and y coordinates
+    parser.add_argument("--initial_x", help="The initial value of the x coordinate", type=float, default=0.0)
+    parser.add_argument("--initial_y", help="The initial value of the y coordinate", type=float, default=0.0)
+
     args = parser.parse_args()
 
-    env = CustomEnv()
+    # Create the custom environment with the initial state
+    env = CustomEnv([args.initial_x, args.initial_y])
+
     env = DummyVecEnv([lambda: env])
     model = get_model(args.model, env)
     model.learn(total_timesteps=args.max_timesteps)
